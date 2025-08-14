be1ee9ea9a70d22aa32ddf8d3969120bc0608685
Дастан Файзулин <anonimych_a@mail.ru>
2025-08-14 21:06:03 +0300

    Change Dockerfile

--- hr_platmiom/hr_platmiom/settings.py
+++ hr_platmiom/hr_platmiom/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'False'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') or [
     '127.0.0.1',
@@ -109,3 +109,5 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATIC_ROOT = BASE_DIR / 'static'
